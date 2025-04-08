493109050761e4cff0044213008d9edacb1837df
Uddu <jagtapuddesh21@gmail.com>
2025-04-08 16:38:49 +0530

    featuretest1

--- src/apnacollage/fea1.java
+++ src/apnacollage/fea1.java
@@ -1,5 +1,11 @@
 package apnacollage;
 
 public class fea1 {
+	
+	public static void main(String[] args) {
 
+		  System.out.println("Hello Uddesh");
+		  
+		  System.out.println("Hello Uddesh");
+}
 }
